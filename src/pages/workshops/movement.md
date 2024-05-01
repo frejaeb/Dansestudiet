6acac9d107add7396cb96b9c233582055d568272
Lisbeth Collin <lico0001@stud.kea.dk>
2024-05-01 13:54:45 +0200

    styling + farve

---
+++ src/pages/workshops/movement.md
@@ -0,0 +1,29 @@
+---
+layout: ../../layouts/WorkshopLayout.astro
+title: "Movement"
+dato: "2024-04-21"
+coverimage: "/nyhed_cover.jpg"
+coveralt: "reduce reuse recycle"
+kortversion: "Kom med til en spændende workshop med Timothy, der nu er aktuel i forestillingen “Askepot the musical”. Workshoppens omdrejningspunkt er “Morph Movement” der sammensmelter mange stilarter og giver ..."
+---
+## Movement
+*med Timothy De Mesa Højgaard*
+
+### Om *workshoppen*
+Workshoppen kommer til at indeholde en  opvarmning, guidet improvisation og noget koreografisk materiale til  slut. Timothy vil give dig redskaber til at finde inspiration og  highlite de værktøjer, som du allerede har, så du kan bruge dem bedst  muligt.
+
+Workshoppens omdrejningspunkt er “Morph  Movement” – at sammensmelte mange forskellige dansestilarter. Så du kan  komme, om du danser house, locking, contemporary eller noget helt  fjerde.
+
+### Vigtig info
+
+- **Niveau:** Alle - 16+
+- **Dato:** Søndag d. 21 april
+- **Tidspunkt:** kl. 10-12
+- **Pris:** 210 kr.
+
+![Marja blev overrasket af kondomer, da hun rodede rundt i glemmekassen](/overrasket.jpg)
+
+### Om *underviseren*
+Timothy  begyndte som gymnast og uddannede sig senere som danser i København. Han  har medvirket i flere tv-produktioner som “Vild med dans”, “X Factor”,  “Danmark har talent” og har også optrådt ved “Copenhagen Fashion Week”.  
+
+Timothy har derudover medvirket som danser og skuespiller i teater- og  musicalforestillinger som “A Jungle Story” på Baltoppen i 2019 og “West  Side Story” i Operaen i 2022. Han har desuden også været aktiv i  contemporary danseprojekter og har mange års erfaring som underviser og  koreograf.
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     plugins: [
@@ -7,4 +8,61 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/container-queries'),
     ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'caption':['"kepler-std-caption"', 'serif'],
+                'body':['"kepler-std"', 'serif'],
+                'display':['"kepler-std-display"', 'serif'],
+                'subheader':['"kepler-std-subhead"', 'serif'],
+                },
+            colors: {
+                'grey': {
+                    50:"#EFEEEC",
+                    100: "#DEDCD8",
+                    200: "#BBB7AF",
+                    300: "#9B9488",
+                    400: "#746E62",
+                    500: "#4D4941",
+                    600: "#3D3A33",
+                    700: "#2F2D28",
+                    800: "#1E1D1A",
+                    900: "#11100E",
+                  },
+                  'blue': {
+                    50:"#E2F5F9",
+                    100: "#C0EAF1",
+                    200: "#86D6E4",
+                    300: "#47C1D7",
+                    400: "#279BB0",
+                    500: "#196572",
+                    600: "#14515C",
+                    700: "#0F3B43",
+                    800: "#0A292E",
+                    900: "#051215",
+                  },
+                  'red': {
+                    50:"#FFE5EA",
+                    100: "#FFCCD5",
+                    200: "#FF9EAE",
+                    300: "#FF6B84",
+                    400: "#FF3859",
+                    500: "#FF0933",
+                    600: "#D10023",
+                    700: "#9E001A",
+                    800: "#6B0012",
+                    900: "#330009",
+                  },
+                  'white': {
+                    100: "#ffffff",
+                  },
+                  'black': {
+                    100: "#0A0A0A",
+                  },
+                  'beige': {
+                    500: "#FCF3E8",
+                  },
+            }
+        }
+    },
   };
